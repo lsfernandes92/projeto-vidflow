c25d82dd48f6e86fc748e44b15e2cdda73954e6c
lsfernandes92 <lsfernandes92@gmail.com>
2024-03-13 18:57:36 -0300

    Refactor the chain async Promise calls

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const containerVideos = document.querySelector(".videos__container")
 
-const api = fetch("http://localhost:3000/videos")
-  .then(response => response.json())
-  .then(videos => {
+async function buscarEMostrarVideos() {
+  try {
+    const busca = await fetch("http://localhost:3000/videos")
+    const videos = await busca.json()
+  
     videos.forEach(video => {
+      if (video.categoria == "") {
+        throw new Error("Algum dos vídeos não tem categoria")
+      }
+
       containerVideos.innerHTML += `
         <li class="videos__item">
           <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
@@ -15,9 +21,9 @@ const api = fetch("http://localhost:3000/videos")
         </li>
       `
     })
-  })
-  .catch(error => {
-    containerVideos.innerHTML = `
-      <p>Houve um erro ao carregar os videos. ${error}<p/>)
-    `
-  })
+  } catch (error) {
+    containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos. ${error}</p>`
+  }
+}
+
+buscarEMostrarVideos()
